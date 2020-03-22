b5ed84e5466befbfd2d4c03f2d92bb27d87b9485
Anna Ambroziak <ambroziak_anna@wp.pl>
2020-03-22 18:18:00 +0100

    fix submit add-comment-form

--- scripts/components/add-comment-form-component.js
+++ scripts/components/add-comment-form-component.js
@@ -2,23 +2,21 @@ function renderAddCommentForm(post, $post) {
     const $div = document.createElement('div');
     const template = `
         <form class="add-comment-form">
-            <input type="text" class="form-control new-comment" placeholder="Napisz komentarz...">
+            <input type="text" class="form-control new-comment" placeholder="Napisz komentarz..." required>
+            <input type="submit" class="hidden"/>
         </form>`;
     $div.innerHTML = template;
     const $comments = $post.querySelector('.comments');
     $comments.appendChild($div);
 
-    const $newComment = $comments.querySelector('.new-comment');
-    $newComment.addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            const id = 'comment-' + Date.now();
-            const value = $newComment.value;
-            if (value) {
-                post.comments.push({ id, value });
-                editPost(post);
-                $newComment.value = '';
-            }
-        }
+    const $form = $comments.querySelector('.add-comment-form');
+    $form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const $newComment = $comments.querySelector('.new-comment');
+        const id = 'comment-' + Date.now();
+        const value = $newComment.value;
+        post.comments.push({ id, value });
+        editPost(post);
+        $newComment.value = '';
     })
 }
